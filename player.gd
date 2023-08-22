f8beb5ecce7707b694a64d8d8a08846aa8e64d1b
Jubibani <strawberryloli3@gmail.com>
2023-08-23 00:02:09 +0800

    INITIALIZE COMMIT.

--- player.gd
+++ player.gd
@@ -10,32 +10,37 @@ var move_vector := Vector2.ZERO
 
 #bobbing
 const BOB_FREQ = 2.0
-const BOB_AMP = 0.05
+const BOB_AMP = 0.05	
 var t_bob = 0.0
 #end of bobbing 
 const MOVE_SPEED = 5.0
-const RUN_SPEED = 9.0
+const RUN_SPEED = 7.0
+const CROUCH_SPEED = 3.5
 const LOOK_SENSITIVITY = 0.1
 
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 3.5
+const WALL_JUMP = 300
+const WALL_SLIDE_FALL = 100
 
+var is_on_wallsliding = false
+
+var can_jump = true  # Flag to track if the character can jump
+var trueSpeed = MOVE_SPEED
+var isCrouching = false
 var sens_horizontal = 0.1
 var sens_vertical = 0.1
 #-----
 var relative   : Vector2
 var fingers    : int
 var raw_gesture : RawGesture
-
 func _init(_raw_gesture : RawGesture = null, event : InputEventScreenDrag = null) -> void:
 	raw_gesture = _raw_gesture
 	if raw_gesture:
 		fingers  = raw_gesture.size()
 		position = raw_gesture.centroid("drags", "position")
 		relative = event.relative/fingers 
-
 func as_string() -> String:
 	return "position=" + str(position) + "|relative=" + str(relative) + "|fingers=" + str(fingers)
-
 #-----
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -62,11 +67,31 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if is_on_wall() and Input.is_action_pressed("forward"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = WALL_JUMP
+		if is_on_wall() and Input.is_action_pressed("backward"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = WALL_JUMP
+		if is_on_wall() and Input.is_action_pressed("ui_right"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = WALL_JUMP
+		if is_on_wall() and Input.is_action_pressed("ui_left"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = WALL_JUMP
+	if Input.is_action_just_pressed("crouch"):
+		if isCrouching == false:
+			movementStateChange("crouch")
+			trueSpeed = CROUCH_SPEED
+			
+		elif isCrouching == true:
+			movementStateChange("uncrouch")
+			trueSpeed = MOVE_SPEED
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
@@ -77,20 +102,65 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
-	
 	# Head bob
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	camera.transform.origin = _headbob(t_bob)
 	# end of head bob
 	move_and_slide()
-
+	wall_slide(delta)
 func _headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
 	pos.y = sin(time*BOB_FREQ)*BOB_AMP
 	pos.x = cos(time*BOB_FREQ)/2 * BOB_AMP
 	return pos
-
-
 func _on_jump_pressed():
 	velocity.y = JUMP_VELOCITY
-	
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		if is_on_wall() and Input.is_action_pressed("forward"):
+			velocity.y = JUMP_VELOCITY
+			velocity.x = WALL_JUMP
+func wall_slide(delta):
+	if is_on_wall() and !is_on_floor():
+		if Input.is_action_just_pressed("forward"):
+			is_on_wallsliding = true
+		else:
+			is_on_wallsliding = false
+	else:
+		is_on_wallsliding = false
+	if is_on_wallsliding:
+		velocity.y += (WALL_SLIDE_FALL * delta)
+		velocity.x = min(velocity.y, WALL_SLIDE_FALL)
+func _on_sprint_pressed():
+	if Input.is_action_pressed("run"):
+		print("running")
+		speed = RUN_SPEED
+	else:
+		print("walking")
+		speed = MOVE_SPEED
+		
+# crouching
+func movementStateChange(changeType):
+	match changeType:
+		"crouch":
+			isCrouching = true
+			$AnimationPlayer.play("StandingToCrouch")
+			changeCollisionShapeTo("crouching")
+		"uncrouch":
+			$AnimationPlayer.play("RESET")
+			isCrouching = false
+			changeCollisionShapeTo("standing")
+#Change collision shapes for standing, crouch
+func changeCollisionShapeTo(shape):
+	match shape:
+		"crouching":
+			#Disabled == false is enabled!
+			$CrouchingCollisionShape3D.disabled = false
+			$StandingCollisionShape3D.disabled = true
+		"standing":
+			#Disabled == false is enabled!
+			$StandingCollisionShape3D.disabled = false
+			$CrouchingCollisionShape3D.disabled = true
+
+
